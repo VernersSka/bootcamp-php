0cda41dc440dc228048a7f7c95c7ee3e05ab4d5a
VernersSka <verners.skalbins@gmail.com>
2021-08-11 16:04:27 +0300

    disabled bot move after winner has been determined

--- four_in_line.php
+++ four_in_line.php
@@ -18,7 +18,8 @@
 	if (
 		array_key_exists('id', $_GET) && 
 		!array_key_exists($_GET['id'], $moves) && // uzspiežot uz aizņemta lauciņa NEIZDARA gājienu
-		in_array($_GET['id'], $legal_moves)				// neļauj izdarīt nelegālu gājienu
+		in_array($_GET['id'], $legal_moves) &&		// neļauj izdarīt nelegālu gājienu
+		!array_key_exists('winner', $moves)				// neļauj izdarīt gājienu, ja uzvarētājs noteikts
 	) {
 		$symbol = count($moves) % 2 == 0 ? 'x' : 'o';
 		$id = $_GET['id'];
@@ -29,21 +30,26 @@
 		) {
 			add($id, $symbol);
 			if (checkWinner($id, $moves)) {
+				add('winner', $symbol);
 				echo "<h2>Winner is '$symbol'!</h2>";
 			}
 		}
 
 		$legal_moves = legal_moves();
-
+		
 		/** Bota gājiens */
-		$bot_move_index = array_rand($legal_moves);
-		$bot_symbol = count($moves) % 2 == 0 ? 'x' : 'o';
+		if (!array_key_exists('winner', $moves)) {
+			$bot_move_index = array_rand($legal_moves);
+			$bot_symbol = count($moves) % 2 == 0 ? 'x' : 'o';
+	
+			add($legal_moves[$bot_move_index], $bot_symbol);
+			$moves = get();
 
-		add($legal_moves[$bot_move_index], $bot_symbol);
-		if (checkWinner($legal_moves[$bot_move_index], $moves)) {
-			echo "<h2>Winner is '$bot_symbol'!</h2>";
+			if (checkWinner($legal_moves[$bot_move_index], $moves)) {
+				add('winner', $bot_symbol);
+				echo "<h2>Winner is '$bot_symbol'!</h2>";
+			}
 		}
-
 	}
 ?>
 
@@ -181,7 +187,3 @@ function getRow($id) {
 	return ($id - $col) / 10;
 }
 ?>
-
-<!-- problēma 
-	- uzspiežot uz nelegālu lauku, bots izdara gājienu
- -->
